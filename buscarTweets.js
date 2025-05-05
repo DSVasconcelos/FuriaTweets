7d24848174ecf857a6aa1f1446c81f6aa5be338c
Danilo S. Vasconcelos <vasconcelosdanilo496@gmail.com>
2025-05-04 22:41:20 -0300

    commit app FuriaTweets

---
+++ buscarTweets.js
@@ -0,0 +1,51 @@
+//import { MongoClient } from 'mongodb';  Se estiver usando .mjs
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://testFuria:testFuria@furia.ht8wkpp.mongodb.net/?retryWrites=true&w=majority&appName=Furia';
+
+const client = new MongoClient(uri);
+
+const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAFZT0wEAAAAAAZcDhovW2cVs3mGnnpLAqa7eMAE%3DxB2Ltk2hNjre1XhhEmjMbH1oO4DjrwAiUsYyZ2Dtg5QOf4spOW';
+const query = '@furia'
+const url = `https://api.twitter.com/2/tweets/search/recent?query=${encodeURIComponent(query)}&max_results=3&tweet.fields=created_at,text,author_id`;
+
+async function buscarTweets(url, bearerToken) {
+  try {
+    const res = await fetch(url, {
+      headers: { Authorization: `Bearer ${bearerToken}` }
+    });
+    const data = await res.json();
+    console.log(data)
+    return data.data || [];
+  } catch (err) {
+    console.error("❌ Erro na busca de tweets:", err);
+    return [];
+  }
+}
+
+async function salvarTweetsNoMongo(tweets) {
+  try {
+    await client.connect();
+    const database = client.db("furiaDB");
+    const collection = database.collection("tweets");
+
+    const result = await collection.insertMany(tweets);
+    console.log(`✅ ${result.insertedCount} tweets salvos no MongoDB`);
+  } catch (err) {
+    console.error("Erro ao salvar no MongoDB:", err.message);
+  } finally {
+    await client.close();
+  }
+}
+
+async function processarTweets() {
+  const tweets = await buscarTweets(url, bearerToken);
+  if (tweets.length > 0) {
+    console.log(`🔍 ${tweets.length} tweets encontrados.`);
+    await salvarTweetsNoMongo(tweets);
+  } else {
+    console.log("❌ Nenhum tweet encontrado.");
+  }
+}
+
+processarTweets();
